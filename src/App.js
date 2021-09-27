22a71d8c8db89b9dc154f86bddc49c181301a043
Henri Hosio <henri.hosio@gmail.com>
2021-09-27 14:57:47 +0300

    update webpack configs, other modules

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
 import { Switch, Router } from "react-router";
 import { Route } from "react-router-dom";
-import { createBrowserHistory } from "history";
 import ScrollToTop from "./Components/ScrollToTop";
 import { TestView1, TestView2 } from "./Views";
 
-const browserHistory = createBrowserHistory();
-require("../favicon.ico");
+import { browserHistory } from "./utils";
 
 const App = () => (
   <Router history={browserHistory}>
